d68fbbe0207137f959f980fdbc2ef4bc8f514158
JohannSchneider1 <johannschneider7881@gmail.com>
2024-03-06 21:39:38 -0300

    Arquivo PrimeiroExemplo.py criado

---
+++ Primeiro Exemplo.py
@@ -0,0 +1,24 @@
+# Comando de saida, para sinalizar a palavra dentro dos ()
+print('hello world')
+
+#Crieando uma variavel numerica e atribuindo um valor
+num = 10
+
+#Imprimindo o valor da variavel num
+print('seu numero é', num)
+
+#trocando o valor da variavel num
+num = -1
+
+print(num)
+
+#Leitura variavel com comando de entrada Nome
+nome = input('Digite o seu nome e precione enter: ')
+print('Então seu nome é:', nome)
+
+#variavel com comando de entrada com numeros com valores inteiros
+idade = int(input('Qual a sua idade? '))
+print('Sua idade é:', idade)
+
+#variavel 
+print('Seu nome é: ', nome, '\nsua idade é: ',idade)
